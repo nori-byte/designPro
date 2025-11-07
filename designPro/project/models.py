42b335a8dad66765f03ca98195b629a4945119f3
nori-byte <kulmanakovap@gmail.com>
2025-11-07 17:29:52 +0700

    whoo

--- designPro/project/models.py
+++ designPro/project/models.py
@@ -11,25 +11,8 @@ class CustomUser(AbstractUser):
     patronymic = models.CharField(max_length=30, blank=True)
     email = models.EmailField(unique=True, verbose_name='Email')
     agreement = models.BooleanField(default=False)
-
-
-    # Добавь related_name чтобы избежать конфликтов
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to.',
-        related_name='customuser_set',
-        related_query_name='user',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name='customuser_set',
-        related_query_name='user',
-    )
+    groups = models.ManyToManyField('auth.Group', verbose_name='groups', blank=True, help_text='The groups this user belongs to.', related_name='customuser_set', related_query_name='user', )
+    user_permissions = models.ManyToManyField('auth.Permission', verbose_name='user permissions',blank=True, help_text='Specific permissions for this user.',related_name='customuser_set',related_query_name='user',)
 
     def __str__(self):
         return self.username
